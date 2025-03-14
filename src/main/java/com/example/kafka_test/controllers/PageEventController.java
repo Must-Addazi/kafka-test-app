a6d636163579381e5cc215592e98bbefe66da7d1
Mustapha Addazi <addazimustapha123@gmail.com>
2025-03-14 18:05:17 +0000

    second commit

--- src/main/java/com/example/kafka_test/controllers/PageEventController.java
+++ src/main/java/com/example/kafka_test/controllers/PageEventController.java
@@ -1,25 +1,34 @@
 package com.example.kafka_test.controllers;
 
 import com.example.kafka_test.events.PageEvent;
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.kstream.Windowed;
+import org.apache.kafka.streams.state.KeyValueIterator;
+import org.apache.kafka.streams.state.QueryableStoreTypes;
+import org.apache.kafka.streams.state.ReadOnlyWindowStore;
+import org.springframework.cloud.stream.binder.kafka.streams.InteractiveQueryService;
 import org.springframework.cloud.stream.function.StreamBridge;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.Duration;
+import java.time.Instant;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import reactor.core.publisher.Flux;
 
 @RestController
 @RequiredArgsConstructor
 public class PageEventController {
 
-    private static final Logger logger = LoggerFactory.getLogger(PageEventController.class);
     private final StreamBridge streamBridge;
-
+    private final InteractiveQueryService interactiveQueryService;
     @GetMapping("/publish")
     public PageEvent publish(@RequestParam String name, @RequestParam String topic) {
         PageEvent pageEvent = new PageEvent(
@@ -29,9 +38,41 @@ public class PageEventController {
                 new Random().nextInt(10000) + 10
         );
 
-        logger.info("Publishing event: {} to topic: {}", pageEvent, topic);
         streamBridge.send(topic, pageEvent);
 
         return pageEvent;
     }
+
+    @GetMapping(path = "/analytics",produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Map<String, Long>> analytics() {
+        return Flux.interval(Duration.ofSeconds(1))
+                .map(sequence -> {
+
+                    Map<String, Long> stringLongMap = new HashMap<>();
+                    ReadOnlyWindowStore<String, Long> windowStore = interactiveQueryService.getQueryableStore("count-store", QueryableStoreTypes.windowStore());
+
+                    // Vérifier si le store est bien récupéré
+                    if (windowStore == null) {
+                        System.out.println("❌ Erreur : count-store est NULL !");
+                        return stringLongMap;
+                    }
+
+                    Instant now = Instant.now();
+                    Instant from = now.minusMillis(5000);
+
+                    // Vérifier si des données existent dans la fenêtre
+                    KeyValueIterator<Windowed<String>, Long> fetchAll = windowStore.fetchAll(from, now);
+                    if (!fetchAll.hasNext()) {
+                        System.out.println("⚠️ Aucune donnée trouvée dans le count-store !");
+                    }
+
+                    while (fetchAll.hasNext()) {
+                        KeyValue<Windowed<String>, Long> next = fetchAll.next();
+                        stringLongMap.put(next.key.key(), next.value);
+                    }
+
+                    return stringLongMap;
+                });
+    }
+
 }
